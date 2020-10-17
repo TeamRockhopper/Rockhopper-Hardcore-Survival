aab4dda8c7a66bfccf5511f00b87d56227f5c7ea
Timothy Clancy <tim@rockhopper.us>
2020-10-17 19:53:32 -0400

    Fixed a typo with SECRET selection.

--- listener-server/app.js
+++ listener-server/app.js
@@ -28,7 +28,7 @@ const verifyPostData = function (req, res, next) {
     return next('Request body empty');
   }
   const sig = req.get(sigHeaderName) || '';
-  const hmac = crypto.createHmac('sha1', secret);
+  const hmac = crypto.createHmac('sha1', SECRET);
   const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8');
   const checksum = Buffer.from(sig, 'utf8');
   if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
