5095a31f052f179f1ef82a65db5ccc03e6771b71
Timothy Clancy <tim@rockhopper.us>
2020-10-18 15:28:12 -0400

    Included listener server logfile.

--- listener-server/app.js
+++ listener-server/app.js
@@ -40,6 +40,9 @@ const verifyPostData = function (req, res, next) {
 };
 
 const restartProcess = function () {
+	const logfile = 'listener-restart.log';
+	const out = fs.openSync(logfile, 'a');
+	const err = fs.openSync(logfile, 'a');
 	spawn(process.argv[1], process.argv.slice(2), {
 		detached: true,
 		stdio: [ 'ignore', out, err ]
