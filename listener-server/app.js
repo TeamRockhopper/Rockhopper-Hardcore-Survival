99d8357d2987e3fed84b0bf046c9b4460b7505b6
Timothy Clancy <tim@rockhopper.us>
2020-10-18 15:53:46 -0400

    Debug attempt 9.

--- listener-server/app.js
+++ listener-server/app.js
@@ -67,7 +67,7 @@ const execShellCommand = function (cmd) {
 app.post('/', verifyPostData, async function (req, res) {
 	let commitId = req.body.after.substring(0, 12);
 	console.log(commitId);
-	console.log(8);
+	console.log(9);
 
 	// Update the repository locally.
 	await execShellCommand('git pull');
