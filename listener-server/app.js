c33df513835fb6b1e2fdd6778723947c8fb184eb
Timothy Clancy <tim@rockhopper.us>
2020-10-18 15:54:46 -0400

    Eureka! Debug attempt 10?

--- listener-server/app.js
+++ listener-server/app.js
@@ -67,7 +67,7 @@ const execShellCommand = function (cmd) {
 app.post('/', verifyPostData, async function (req, res) {
 	let commitId = req.body.after.substring(0, 12);
 	console.log(commitId);
-	console.log(9);
+	console.log(10);
 
 	// Update the repository locally.
 	await execShellCommand('git pull');
